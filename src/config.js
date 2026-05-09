6902e170b1b6e9093faf7a8cc309125949a88c85
Biji Sukumar <bijisukumar@gmail.com>
2026-05-09 22:32:10 +0000

    Config: Apps Script URL V12

--- src/config.js
+++ src/config.js
@@ -12,7 +12,7 @@ export const CONFIG = {
   tagline:    'Property Management Portal',
 
   // Google integration
-  appsScriptUrl: 'https://script.google.com/macros/s/AKfycbxOmeJUo_cVtJq7E-ZCjojrNrnlh3iBtKE4PgsJuAbzyi7Z1PerG8NaNKLcnMIx5N2b/exec',
+  appsScriptUrl: 'https://script.google.com/macros/s/AKfycby3cQFxtjGj3Aeu-abFYy_i7rPOS_m9REsaNoQbF2HnN7rN6zfe7LR2aXfveBZh8hDp/exec',
   driveRootId:   '1Qyy37HJVo4RQ5MPVmSJt26-SkE65sFva',
   ownerEmail:    'bijisukumar@gmail.com',
   spreadsheetId:    '1xpLBxd2Fhx26aNQZ3Z5L4gDB6yJVFsGHf3B1jUDkvQQ',  // add this

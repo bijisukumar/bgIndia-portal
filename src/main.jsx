6d07402c7d2da05985d976663c00455c69a3526f
Bijit <kerala.luxuryvillas@gmail.com>
2026-06-06 00:45:32 -0500

    fix: revert main.jsx to simple manage app import

--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,9 @@
-import React, { Suspense, lazy } from 'react'
+import React from 'react'
 import ReactDOM from 'react-dom/client'
-
-const host = window.location.hostname
-
-const App = host.startsWith('stayvibe')
-  ? lazy(() => import('./apps/stayvibe/App.jsx'))
-  : host.startsWith('rev360')
-  ? lazy(() => import('./apps/rev360/App.jsx'))
-  : host.startsWith('estate360')
-  ? lazy(() => import('./apps/estate360/App.jsx'))
-  : lazy(() => import('./apps/manage/App.jsx'))
+import App from './apps/manage/App'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Suspense fallback={null}>
-      <App />
-    </Suspense>
+    <App />
   </React.StrictMode>
 )
